27f0a5d49f913aeda46acc672d642c7d295e02e7
MANOJ KUMAR S <smk11500@gmail.com>
2022-11-11 00:17:54 +0530

    result page

--- Final Deliverables/app/app.py
+++ Final Deliverables/app/app.py
@@ -4,7 +4,6 @@ from flask import Flask, request, redirect, flash, send_from_directory, render_t
 
 app = Flask(__name__, static_url_path='/')
 app.config['SECRET_KEY'] = 'secret_key'
-app.config['UPLOADS_FOLDER'] = './static/'
-folder = app.config['UPLOADS_FOLDER']
+app.config['UPLOAD_FOLDER'] = './static/uploads/'
 
 
--- Final Deliverables/app/main.py
+++ Final Deliverables/app/main.py
@@ -1,4 +1,6 @@
 from app import *
+import os
+from utils.predict import predict
 
 @app.route("/", methods=["GET"])
 def index():
@@ -23,7 +25,14 @@ def upload():
     if request.method=="GET":      
         return render_template('upload.html')
     if request.method=="POST":
-        pass
+        imgType = request.form["type"]
+        file = request.files['file']
+
+        imgPath = os.path.join(app.config['UPLOAD_FOLDER'], "upImage.jpg")
+        file.save(imgPath)
+
+        prediction = predict(imgPath, imgType)
+        return render_template('result.html', prediction=prediction, path=imgPath)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
